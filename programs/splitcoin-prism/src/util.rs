28a491d5cd03f8be02925daa25b4cbbc383fae85
Kevin Li <kevinli29033@gmail.com>
2022-02-08 00:23:11 -0500

    Added get dex price

--- programs/splitcoin-prism/src/util.rs
+++ programs/splitcoin-prism/src/util.rs
@@ -1,4 +1,5 @@
 use crate::asset_data::{AssetData, ReadablePrice};
+use serum_dex::critbit::Slab;
 
 // TODO replace 8 with shared max size
 /// Helper function that returns Prism token's value
@@ -8,4 +9,17 @@ pub fn token_value(asset_data: &[AssetData; 8]) -> i64 {
         sum += asset.data_feed.get_price().price * asset.weight;
     });
     sum
+}
+
+pub fn get_dex_price(bids: &Slab) -> u64 {
+    for node in bids.nodes().iter() {
+        match node.as_leaf() {
+            Some(leaf) => {
+                return leaf.price().get();
+            }
+            _ => {}
+        }
+    }
+
+    return 0;
 }
