d3ca9ea3293412dc20eb4a961bcfd458e6bc3f5a
Kevin Li <kevinli29033@gmail.com>
2022-04-26 15:36:10 -0400

    bump

--- src/CoherenceApi/model.ts
+++ src/CoherenceApi/model.ts
@@ -5,6 +5,19 @@ export interface UserPrismEtf {
   icon?: string;
   info?: string;
   listingBid: number;
-  targetAllocations: { mint: string; target: number }[];
+  targetAllocations: {
+    mint: string;
+    target: number;
+    coingeckoInfo: {
+      id: string;
+      market_data: {
+        current_price: { usd: number };
+        price_change_24h: number;
+        price_change_percentage_24h: number;
+      };
+    };
+  }[];
   highlighted: boolean;
+  aum: number;
+  supply: number;
 }
